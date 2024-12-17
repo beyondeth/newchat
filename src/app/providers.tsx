15b23e8c694805ac5e2ac355b7de8a8a4e668cdf
park992 <luticek@naver.com>
2024-12-17 21:58:32 +0900

    posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,3 +1,4 @@
+// app/providers.tsx
 "use client";
 
 import posthog from "posthog-js";
@@ -5,20 +6,12 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
 import PostHogPageView from "./PostHogPageView";
 
-if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-  throw new Error("NEXT_PUBLIC_POSTHOG_KEY is not defined");
-}
-
-if (!process.env.NEXT_PUBLIC_POSTHOG_HOST) {
-  throw new Error("NEXT_PUBLIC_POSTHOG_HOST is not defined");
-}
-
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: "identified_only",
-      capture_pageview: false,
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     });
   }, []);
 
