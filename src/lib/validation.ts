37c55fc2234cafa86c47f3ab2f0f696da16785e3
park992 <luticek@naver.com>
2025-01-23 22:01:14 +0900

    content minimum

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -35,7 +35,6 @@ export const createPostSchema = z.object({
   content: z
     .string()
     .trim()
-    .min(10, "본문 내용은 최소 10자 이상 입력해주세요.")
     .max(3000, "본 문 내용은 최대 3000자까지 입력가능합니다."),
   mediaIds: z.array(z.string()).max(5, "5개까지만 업로드 가능합니다"),
   booktitle: z
