0f60167d34446b47ff0426aef9e26a6317ba45b7
park992 <luticek@naver.com>
2025-01-28 02:34:21 +0900

    mutations2

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -980,7 +980,7 @@ export function getPostDataInclude(loggedInUserId: string) {
       select: getUserDataSelect(loggedInUserId),
     },
     attachments: true,
-    bookInfo: true, // bookInfo로 이름 변경
+
     likes: {
       where: {
         userId: loggedInUserId,
@@ -1015,38 +1015,9 @@ export function getPostDataInclude(loggedInUserId: string) {
   } satisfies Prisma.PostInclude;
 }
 
-export type BookInfoData = BookInfo;
-
 export type PostData = Prisma.PostGetPayload<{
   include: ReturnType<typeof getPostDataInclude>;
-}> & {
-  bookInfo: {
-    id: string;
-    title: string;
-    author: string;
-    image: string;
-    publisher: string;
-    pubdate: string;
-    isbn: string;
-    description: string;
-    link: string | null; // null 허용으로 변경
-    createdAt: Date; // 필수 필드 추가
-    updatedAt: Date; // 필수 필드 추가
-    postId: string; // 필수 필드 추가
-  } | null; // nullable로 변경
-};
-
-export interface BookInfo {
-  id?: string; // DB에 저장될 때 필요
-  title: string;
-  author: string;
-  image: string;
-  publisher: string;
-  pubdate: string;
-  isbn: string;
-  description: string;
-  link?: string | null; // undefined 대신 null로 변경 // 선택적 필드
-}
+}>;
 
 export interface PostsPage {
   posts: PostData[];
